ce035e2523413cd1a36072ce0937ab4c9f721375
Okhotsk-hr <141467684+Okhotsk-hr@users.noreply.github.com>
2024-12-13 20:38:28 +0900

    青の光を追加

--- main.ts
+++ main.ts
@@ -1,15 +1,27 @@
-input.onButtonPressed(Button.A, function () {
-    sensor = 0
-})
-input.onButtonPressed(Button.B, function () {
-    sensor = 1
-})
+function red_flash () {
+	
+}
+function blue_flash () {
+    i = 0
+    for (let index = 0; index < 8; index++) {
+        strip.setPixelColor(i, neopixel.colors(NeoPixelColors.Blue))
+        strip.show()
+        i += 1
+        basic.pause(10)
+    }
+    basic.pause(1000)
+}
 function Flash_draw () {
     strip.showColor(neopixel.colors(NeoPixelColors.Black))
     i = 0
-    for (let index = 0; index < 8; index++) {
-        strip.setPixelColor(i - 1, neopixel.colors(NeoPixelColors.Black))
-        strip.setPixelColor(i, neopixel.colors(NeoPixelColors.Red))
+    for (let index = 0; index < 15; index++) {
+        color = 200
+        j = 0
+        for (let index = 0; index < 7; index++) {
+            strip.setPixelColor(i - j, neopixel.rgb(50, 0, color))
+            color += -10
+            j += 1
+        }
         strip.show()
         basic.pause(100)
         i += 1
@@ -17,20 +29,13 @@ function Flash_draw () {
     strip.showColor(neopixel.colors(NeoPixelColors.Black))
     basic.pause(2000)
 }
-let IR = 0
+let j = 0
+let color = 0
 let i = 0
-let sensor = 0
 let strip: neopixel.Strip = null
 strip = neopixel.create(DigitalPin.P2, 8, NeoPixelMode.RGB)
 let range = strip.range(0, 8)
-strip.setBrightness(100)
+strip.setBrightness(255)
 basic.forever(function () {
-    // 赤外線センサー：ドロー判定
-    // P0
-    if (pins.analogReadPin(AnalogPin.P0) < 20) {
-        IR = 1
-        Flash_draw()
-    } else {
-        strip.showColor(neopixel.colors(NeoPixelColors.Black))
-    }
+	
 })
